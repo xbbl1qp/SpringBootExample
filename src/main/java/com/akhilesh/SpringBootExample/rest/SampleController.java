31b1424a2de13176fac632f87b7846877edd9651
asingh <asingh@alpha-sense.com>
2023-03-28 20:51:45 +0530

    test

--- src/main/java/com/akhilesh/SpringBootExample/rest/SampleController.java
+++ src/main/java/com/akhilesh/SpringBootExample/rest/SampleController.java
@@ -1,2 +1,14 @@
-package com.akhilesh.SpringBootExample.rest;public class SampleController {
+package com.akhilesh.SpringBootExample.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @GetMapping("/getName")
+    public String getName(){
+        return "Hi!! This is Akhilesh.";
+    }
+
 }
